1706ad9aab320b79900714a9e66bc43e5b9a60ac
devriazul <engr.riazul@gmail.com>
2021-11-09 22:00:15 +0600

    V2

--- hooks/footer-extras.php
+++ hooks/footer-extras.php
@@ -2,5 +2,5 @@
     <div class="pull-right hidden-xs">
       <b>Version</b> 1.1.0
     </div>
-    <strong>Libray Management System &nbsp;Brought To You By <a href="https://code-projects.org/"> Code-Projects</a>.</strong>
+    <strong>Libray Management System &nbsp;Brought To You By <a href="https://riaz.fastitbd.com">Md Riazul Islam</a>.</strong>
   </footer>
